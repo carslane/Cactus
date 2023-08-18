b344c8d6a22d2ef570b7b14b8813a482c86d1fb6
carslane <142292962+carslane@users.noreply.github.com>
2023-08-19 00:10:36 +0530

    Update js.js

--- Feelings/js.js
+++ Feelings/js.js
@@ -5,6 +5,13 @@ marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]);
+
+// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]);
+
 
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
